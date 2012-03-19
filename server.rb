d4da6a0b1c29a7e3590ff04f4ab89881225e2844
Mikko Ruokojoki <mikko@ruokojoki.fi>
2012-03-19 23:05:41 +0200

    problem ith cross domain

--- server.rb
+++ server.rb
@@ -1,34 +1,8 @@
 require 'sinatra'
 require 'json'
 
-get '/ex1' do
-  File.read("ex1.html")
-end
+set :public_folder, settings.root
 
-get '/ex2' do
-  File.read("ex2.html")
-end
-
-get '/ex3' do
-  File.read("ex3.html")
-end
-
-get '/ex4' do
-  File.read("ex4.html")
-end
-
-get '/ex5' do
-  File.read("ex5.html")
-end
-
-get '/ex6' do
-  File.read("ex6.html")
-end
-
-get '/ex7' do
-  File.read("ex7.html")
-end
-
-get '/ex8' do
-  File.read("ex8.html")
+get '/' do
+  File.read("index.html")
 end
