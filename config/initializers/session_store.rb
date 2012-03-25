266a111a9889b5c824be5e95a61e26ff095ec1b5
Mikko Ruokojoki <mikko@ruokojoki.fi>
2012-03-25 20:51:03 +0300

    session change.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-FeelTask::Application.config.session_store :cookie_store, key: '_FeelTask_session'
+FeelTask::Application.config.session_store :cookie_store, :key => '_feeltask_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
