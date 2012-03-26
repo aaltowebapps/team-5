eafcf9748918e3c6b8e58ee1da4b87f852e05bf5
Mikko Ruokojoki <mikko@ruokojoki.fi>
2012-03-26 15:44:55 +0300

    adding some sensors.

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -10,6 +10,23 @@ var loadTodos = function () {
   $('#todos').append(output).find('ul').listview('refresh');
 };
 
+function onDeviceMotion(event) {
+  var ctx = document.getElementById("c").getContext("2d");
+  var accel = event.accelerationIncludingGravity;
+  $("#sliderX").val(Math.round(accel.x)).slider("refresh");
+  $("#sliderY").val(Math.round(accel.y)).slider("refresh");
+  $("#sliderZ").val(Math.round(accel.z)).slider("refresh");
+  var angle = Math.atan2(accel.y, accel.x)
+  ctx.clearRect(0, 0, 100, 100);
+  ctx.beginPath();
+  ctx.arc(50, 50, 5, 0, 2 * Math.PI, false);
+  ctx.moveTo(50, 50);
+  ctx.lineTo(50 - 50 * Math.cos(angle), 50 + 50 * Math.sin(angle));
+  ctx.stroke();
+}
+
 $(document).ready(function () {
+  window.addEventListener("devicemotion", onDeviceMotion, false);
   loadTodos();
 });
+
