52de39e85e6fd4dd930120f6fa6b5f8a4c875400
Mikko Ruokojoki <mikko@ruokojoki.fi>
2012-04-05 22:47:52 +0300

    Added precompile removings after vmc update.

--- update_feeltask_vmc.sh
+++ update_feeltask_vmc.sh
@@ -1,8 +1,10 @@
 #bin/sh
-echo Updating Feeltask
+echo Updating Feeltask now!
 echo Precompiling assets
 rm -rf public/assets
 RAILS_ENV=production rake assets:precompile
 echo Updating feeltask on cloudfounry...
 vmc update feeltask
+echo Removing precompilations as they mess up deveplopent env...
+rm -rf public/assets
 echo All done!
