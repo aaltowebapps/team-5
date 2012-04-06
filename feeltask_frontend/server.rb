ae6d8f2f10b29fe05a0c840d540767efc1efaf8b
Mikko Ruokojoki <mikko@ruokojoki.fi>
2012-04-06 13:00:29 +0300

    disabled manifest for quickier development.

--- feeltask_frontend/server.rb
+++ feeltask_frontend/server.rb
@@ -2,10 +2,11 @@ require 'sinatra'
 require 'json'
 require 'haml'
 
-get '/manifest.appcache' do
-  headers 'Content-Type' => 'text/cache-manifest' # Must be served with this MIME type
-  send_file 'manifest.appcache'
-end
+# Disabled manifest for quickier development.
+#get '/manifest.appcache' do
+#  headers 'Content-Type' => 'text/cache-manifest' # Must be served with this MIME type
+#  send_file 'manifest.appcache'
+#end
 
 get '/' do
   haml :index, :layout => :layout
