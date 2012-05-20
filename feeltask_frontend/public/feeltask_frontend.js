39610ecc3b3044f2806e25a5459400b36781decc
Edward Landtman <edward.landtman@aalto.fi>
2012-05-20 21:42:54 +0300

    map page added

--- feeltask_frontend/public/feeltask_frontend.js
+++ feeltask_frontend/public/feeltask_frontend.js
@@ -248,6 +248,7 @@ function initViews() {
         var id = target.data("id");
         var item = this.collection.get(id);
         console.debug("Clicked map for item id " + item.get("id"));
+        $.mobile.changePage("#map");
         // TODO: Implement map show
       }
     },
